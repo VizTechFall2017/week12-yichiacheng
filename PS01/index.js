e3ba25fc0e6cab6948c5551ae504b1b5ef434e3f
yichiacheng <31904793+yichiacheng@users.noreply.github.com>
2017-12-04 22:52:14 -0500

    PS01

--- PS01/index.js
+++ PS01/index.js
@@ -14,7 +14,7 @@ var g = svg.append("g")
 var nestedData = [];
 
 
-var marginLeft = 30;
+var marginLeft = 100;
 var marginTop = 80;
 
 var svg2 = d3.select('#svg2')
@@ -35,6 +35,7 @@ var densityLookup = d3.map();
 var crimerateLookup = d3.map();
 var tstationLookup = d3.map();
 var descriptionLookup = d3.map();
+//var dotLookup= d3.map();
 
 
 path = d3.geoPath()
@@ -47,13 +48,13 @@ d3.queue()
     .defer(d3.csv,"./brent20171615.csv")
     .await(function(err, mapData, districtData,dataIn){
 
-console.log(districtData);
+//console.log(districtData);
 
 //d3.csv("./brent20171615.csv", function(dataIn) {
     nestedData = d3.nest()
         .key(function(d){return d.year})
         .entries(dataIn);
-    console.log(nestedData);
+    //console.log(nestedData);
 
     var loadData = nestedData.filter(function(d){return d.key == '2017'})[0].values;
 
@@ -103,7 +104,7 @@ console.log(districtData);
 
 
 
-        console.log(districtData);
+       // console.log(districtData);
         districtData.forEach(function(d){
             densityLookup.set(d.neighborhoods, d.density);
             crimerateLookup.set(d.neighborhoods, d.crimerate);
@@ -111,10 +112,10 @@ console.log(districtData);
             descriptionLookup.set(d.neighborhoods, d.description);
         });
         // set: what this library's entry should be
-        console.log(densityLookup);
-        console.log(crimerateLookup);
-        console.log(tstationLookup);
-        console.log(descriptionLookup);
+       // console.log(densityLookup);
+        //console.log(crimerateLookup);
+       // console.log(tstationLookup);
+        //console.log(descriptionLookup);
 
 
 
@@ -133,14 +134,228 @@ console.log(districtData);
 
 
             .on('mouseover',function(d){
-                console.log(d.properties.Name);
+              //  console.log(d.properties.Name);
                 document.getElementById('title').innerHTML = d.properties.Name;
                 document.getElementById('crimerate').innerHTML = (crimerateLookup.get(d.properties.Name));
                 document.getElementById('density').innerHTML = (densityLookup.get(d.properties.Name));
                 document.getElementById('tstation').innerHTML = (tstationLookup.get(d.properties.Name));
                 document.getElementById('description').innerHTML = (descriptionLookup.get(d.properties.Name));
+
+
+                d3.select(this)
+                    .attr('fill', 'slategray');
+                })
+                    .on('mouseout', function(d) {
+                        d3.select(this)
+                            .attr('fill', 'gainsboro');
+                    });
+
+
+                Arraylist1=[
+                    {long:-71.0622954369, lat:42.3561971861},
+                    {long:-71.0648703575, lat:42.3523914894},
+                    {long:-71.070498, lat:42.351868},
+                    {long:-71.078089, lat:42.349962},
+                    {long:-71.088396, lat:42.348097},
+                    {long:-71.0815858841, lat:42.3456358101},
+                    {long:-71.085095, lat:42.342697},
+                    {long:-71.0888922215, lat:42.3403227355},
+                    {long:-71.0954797268, lat:42.3377215443},
+                    {long:-71.100652, lat:42.3377215443},
+                    {long:-71.059895, lat:42.359297},
+                    {long:-71.058996, lat:42.362498},
+                    {long:-71.061423, lat:42.365512},
+                    {long:-71.0681641102, lat:42.3666775177},
+                    //{long:-71.076884, lat:42.370582},
+                    {long:-71.095296, lat:42.348797},
+                    {long:-71.1043953896, lat:42.3452869056},
+                    {long:-71.100796, lat:42.349297},
+                    {long:-71.103825, lat:42.349648},
+                    {long:-71.1043953896, lat:42.3499335211},
+
+                    {long:-71.1140727997, lat:42.350900862},
+                    {long:-71.1159074306, lat:42.3511308015},
+                    {long:-71.1182141304, lat:42.3513448824},
+                    {long:-71.1212611198, lat:42.3517413265},
+                    {long:-71.1248660088, lat:42.3520743376},
+                    {long:-71.1310243607, lat:42.3502348256},
+                    {long:-71.1341571808, lat:42.3487124302},
+                    {long:-71.1377835274, lat:42.3484428354},
+                    {long:-71.1043953896, lat:42.3484745525},
+                    {long:-71.1428689957, lat:42.3436850916},
+                    {long:-71.1043953896, lat:42.3414964086},
+                    {long:-71.1513018608, lat:42.3403385961},
+                    {long:-71.1577820778, lat:42.3395772832},
+                    {long:-71.1661934853, lat:42.3399420801},
+                    {long:-71.1533403397, lat:42.338086352}];
+
+                    Arraylist2=[
+//orange
+                    {long:-71.060788, lat:42.355295},
+                    {long:-71.062892, lat:42.352228},
+                    {long:-71.063795, lat:42.349873},
+                    {long:-71.0760390759, lat:42.3472772215},
+                    {long:-71.0832166672, lat:42.3415519196},
+                    {long:-71.0905230045, lat:42.3356674788},
+                    {long:-71.0954046249, lat:42.3315274209},
+                    {long:-71.1000823975, lat:42.3227388088},
+                    {long:-71.1028289795, lat:42.3192008078},
+                    {long:-71.107313633, lat:42.3105691548},
+                    {long:-71.113411, lat:42.300362}];
+
+                    Arraylist3=[
+//red
+                    {long:-71.0549998283, lat:42.351709611},
+                    {long:-71.05713, lat:42.3429},
+                    {long:-71.05696, lat:42.32955},
+                    {long:-71.0523927212, lat:42.3214378629},
+                    {long:-71.0532295704, lat:42.311307017},
+                    {long:-71.0607075691, lat:42.3002619819},
+                    {long:-71.0657823086, lat:42.2927943769},
+                    {long:-71.064219, lat:42.285924},
+                    {long:-71.0597419739, lat:42.278420118},
+                    {long:-71.0720801353, lat:42.3612710899}];
+
+                    Arraylist4=[
+//blue
+                    {long:-71.062129, lat: 42.361457},
+                    {long:-71.05357, lat:42.359456},
+                    {long:-71.039926, lat:42.36886},
+                    {long:-71.035194397, lat:42.3727334327},
+                    {long:-71.023394, lat:42.380797},
+                    {long:-71.006628, lat:42.386676},
+                    {long:-71.0003578663, lat:42.3884015915}
+
+
+
+
+                ];
+                svg2.selectAll('circle1')
+                    .data(Arraylist1)       //bind a single data point, with the long lat of Boston
+                    //note that long is negative because it is a W long point!
+                    .enter()
+                    .append('circle')
+                    .attr('cx', function (d){
+                        return albersProjection([d.long, d.lat])[0]
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.long, d.lat])[1]
+                    })
+                    .attr('r', 3)
+                    .attr('fill','black')
+                    .on('mouseover', function(d){
+
+
+                        d3.select(this).attr("class","incident hover");
+                        d3.select(this).attr("fill","forestgreen");
+
+
+
+                    })
+                    .on("mouseout", function(d){
+                        d3.select(this).attr("fill",'black');})
+
+
+                svg2.selectAll('circle2')
+                    .data(Arraylist2)       //bind a single data point, with the long lat of Boston
+                    //note that long is negative because it is a W long point!
+                    .enter()
+                    .append('circle')
+                    .attr('cx', function (d){
+                        return albersProjection([d.long, d.lat])[0]
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.long, d.lat])[1]
+                    })
+                    .attr('r', 3)
+                    .attr('fill','black')
+                    .on('mouseover', function(d){
+
+
+                        d3.select(this).attr("class","incident hover");
+                        d3.select(this).attr("fill","darkorange");
+
+
+
+                    })
+                    .on("mouseout", function(d){
+                        d3.select(this).attr("fill",'black');})
+
+
+
+
+
+                svg2.selectAll('circle3')
+                    .data(Arraylist3)       //bind a single data point, with the long lat of Boston
+                    //note that long is negative because it is a W long point!
+                    .enter()
+                    .append('circle')
+                    .attr('cx', function (d){
+                        return albersProjection([d.long, d.lat])[0]
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.long, d.lat])[1]
+                    })
+                    .attr('r', 3)
+                    .attr('fill','black')
+                    .on('mouseover', function(d){
+
+
+                        d3.select(this).attr("class","incident hover");
+                        d3.select(this).attr("fill","red");
+
+
+
+                    })
+                    .on("mouseout", function(d){
+                        d3.select(this).attr("fill",'black');})
+
+
+                svg2.selectAll('circle4')
+                    .data(Arraylist4)       //bind a single data point, with the long lat of Boston
+                    //note that long is negative because it is a W long point!
+                    .enter()
+                    .append('circle')
+                    .attr('cx', function (d){
+                        return albersProjection([d.long, d.lat])[0]
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.long, d.lat])[1]
+                    })
+                    .attr('r', 3)
+                    .attr('fill','black')
+                    .on('mouseover', function(d){
+
+
+                        d3.select(this).attr("class","incident hover");
+                        d3.select(this).attr("fill","royalBlue");
+
+
+
+                    })
+                    .on("mouseout", function(d){
+                        d3.select(this).attr("fill",'black');})
+
+
+
+
             });
-    });
+               /* svg2.selectAll('circle')
+                    .data(dotData.features);
+                    console.log(dotData)
+                    .enter()
+                    .append('circle')
+                   // .attr('id', function(d){return d.station;})
+                    .attr('cx', function (d){
+                        return albersProjection([d.longtitude, d.latitude])[0]
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.longtitude, d.latitude])[1]
+                    })
+                    .attr('r', 3)
+                    .attr('fill','navvy');
+            });*/
+   // });
 
 
 
@@ -205,9 +420,9 @@ function drawPoints(pointData){console.log(pointData);
 
 
 function updateData(selectedYear){
-    console.log(selectedYear);
+    //console.log(selectedYear);
     var loadData = nestedData.filter(function(d){return d.key == selectedYear})[0].values;
-    console.log(loadData);
+    //console.log(loadData);
     drawPoints(loadData)
 }
 
